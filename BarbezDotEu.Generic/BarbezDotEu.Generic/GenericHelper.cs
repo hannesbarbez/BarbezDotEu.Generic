4a24a78e3e2df5805b5d94f465a0aa4a258e384d
Hannes Barbez <hannesbarbez@hotmail.com>
2020-09-05 12:39:43 +0200

    Initial commit;

---
+++ BarbezDotEu.Generic/BarbezDotEu.Generic/GenericHelper.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) Hannes Barbez. All rights reserved.
+// Licensed under the GNU General Public License v3.0
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BarbezDotEu.Generic
+{
+    /// <summary>
+    /// Borrowed from https://stackoverflow.com/questions/2019417/how-to-access-random-item-in-list.
+    /// </summary>
+    public static class GenericHelper
+    {
+        public static T PickRandom<T>(this IEnumerable<T> source)
+        {
+            return source.PickRandom(1).Single();
+        }
+
+        public static IEnumerable<T> PickRandom<T>(this IEnumerable<T> source, int count)
+        {
+            return source.Shuffle().Take(count);
+        }
+
+        public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> source)
+        {
+            return source.OrderBy(x => Guid.NewGuid());
+        }
+    }
+}
